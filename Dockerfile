bea68cc84463c6d2c18a6687630ace4a09050291
Jonathan Meza <practicantesena3@ticsocial.com.co>
2023-02-09 14:14:10 -0500

    task model created

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM python:3.10.9-alpine3.17
+FROM python:3.9.16-alpine3.17
 
 ENV PYTHONUNBUFFERED 1
 
-COPY ./requirements.txt ./
-RUN python -m pip install -r /requirements.txt
-
-RUN mkdir /app
 WORKDIR /app
-COPY ./app /app
+
+COPY ./requirements.txt ./
+RUN python -m pip install -r requirements.txt
+
+COPY ./ ./
 
 RUN adduser -D user
 USER user
---
+++ drf/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User 
+
+status = [
+  (1, 'BACKLOG'),
+  (2, 'TO DO'),
+  (3, 'DOING'),
+  (4, 'TEST'),
+  (5, 'DONE')
+]
+
+issue = [
+  (1, 'HIGH'),
+  (2, 'MEDIUM'),
+  (1, 'LOW')
+]
+
+
+# Create your models here.
+class Drf(models.Model):
+  title = models.CharField(max_length=200) 
+  description = models.TextField(blank=True)                       
+  state = models.IntegerField(null=False, blank=False, choices=status)
+  priority = models.IntegerField(null=False, blank=False, choices=issue)
+  deliver_date = models.DateTimeField(auto_now_add=False)
+  comment = models.TextField(blank=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+  def __str__(self):
+    return self.title  + ' by ' + self.user.username
+
+
