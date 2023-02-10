641f9430cd0ce31cd90902bf0c14dcdbdab948b5
Jonathan Meza <practicantesena3@ticsocial.com.co>
2023-02-10 14:10:57 -0500

    apiview and filter

--- drf/api.py
+++ drf/api.py
@@ -1,10 +1,12 @@
 from .models import Drf
 from rest_framework import viewsets, permissions
 from .serializers import DrfSerializer
+from rest_framework import filters
 
 class DrfViewSet(viewsets.ModelViewSet):
-  queryset = Drf.objects.all()
+  queryset = Drf.objects.all().order_by('-deliver_date')
   permission_classes = [permissions.AllowAny]
   serializer_class = DrfSerializer
-
+  filter_backends = [filters.SearchFilter]
+  search_fields = ['title', 'state', 'priority', 'deliver_date']
 
