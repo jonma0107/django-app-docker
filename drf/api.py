8398d90969c0e67f491321bfcf1cde3d750f060c
Jonathan Meza <practicantesena3@ticsocial.com.co>
2023-02-13 14:04:41 -0500

    to filter state and priority

--- drf/api.py
+++ drf/api.py
@@ -2,7 +2,7 @@ import django_filters.rest_framework
 from .models import Drf
 from rest_framework import viewsets, permissions
 from .serializers import DrfSerializer
-from rest_framework import filters, generics
+from rest_framework import filters
 
 class DrfViewSet(viewsets.ModelViewSet):
   queryset = Drf.objects.all().order_by('-deliver_date')
@@ -10,6 +10,6 @@ class DrfViewSet(viewsets.ModelViewSet):
   serializer_class = DrfSerializer
   # filter_backends = [filters.SearchFilter]
   filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-  search_fields = ['title', 'state', 'deliver_date']
+  search_fields = ['title', 'state', 'priority', 'deliver_date']
   ordering_fields = ['deliver_date']
 
--- drf/models.py
+++ drf/models.py
@@ -1,27 +1,36 @@
 from django.db import models
-from django.contrib.auth.models import User 
+# from django.contrib.auth.models import User 
 
-status = [
-  (1, 'BACKLOG'),
-  (2, 'TO DO'),
-  (3, 'DOING'),
-  (4, 'TEST'),
-  (5, 'DONE')
-]
+# status = [
+#   (1, 'BACKLOG'),
+#   (2, 'TO DO'),
+#   (3, 'DOING'),
+#   (4, 'TEST'),
+#   (5, 'DONE')
+# ]
 
-issue = [
-  (1, 'HIGH'),
-  (2, 'MEDIUM'),
-  (3, 'LOW')
-]
+# issue = [
+#   (1, 'HIGH'),
+#   (2, 'MEDIUM'),
+#   (3, 'LOW')
+# ]
+
+status = [  ('BACKLOG', 'BACKLOG'),  ('TO DO', 'TO DO'),  ('DOING', 'DOING'),  ('TEST', 'TEST'),  ('DONE', 'DONE')]
+
+issue = [  ('HIGH', 'HIGH'),  ('MEDIUM', 'MEDIUM'),  ('LOW', 'LOW')]
+
 
 
 # Create your models here.
 class Drf(models.Model):
   title = models.CharField(max_length=200) 
   description = models.TextField(blank=True)                       
-  state = models.IntegerField(null=False, blank=False, choices=status)
-  priority = models.IntegerField(null=False, blank=False, choices=issue)
+  # state = models.IntegerField(null=False, blank=False, choices=status)
+  # priority = models.IntegerField(null=False, blank=False, choices=issue)
+
+  state = models.CharField(max_length=20, null=False, blank=False, choices=status)
+  priority = models.CharField(max_length=20, null=False, blank=False, choices=issue)
+
   deliver_date = models.DateField(auto_now_add=False)
   comment = models.TextField(blank=True)
   # user = models.ForeignKey(User, on_delete=models.CASCADE)
